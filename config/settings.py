0949212a642bd331d3d0c25dec4d1c62b551788e
hacker <naimov.bunyod2021@gmail.com>
2023-05-05 15:56:19 +0500

    start_project

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=120),
-    "REFRESH_TOKEN_LIFETIME": timedelta(seconds=360),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=360),
     "UPDATE_LAST_LOGIN": True,
 }
